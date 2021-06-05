833eca253fe34494cf55883bdbeb38769bb8cba8
Akshay Trivedi <aku24.7x3@gmail.com>
2021-06-05 14:24:28 -0700

    test window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let event_loop = winit::event_loop::EventLoop::new();
+    let window = winit::window::WindowBuilder::new()
+        .with_inner_size(winit::dpi::LogicalSize::new(1280, 800))
+        .with_title("sex lol")
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        winit::event::Event::WindowEvent { event, .. } => match event {
+            winit::event::WindowEvent::CloseRequested => {
+                *control_flow = winit::event_loop::ControlFlow::Exit
+            }
+            _ => ()
+        }
+        _ => ()
+    })
 }
